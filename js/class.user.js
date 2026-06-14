6287212e46d54c0d5012d996d8cc0f0bff0b626b
maikypi <s2510892022@fhooe.at>
2026-06-14 15:13:58 +0200

    Update class.user.js

--- js/class.user.js
+++ js/class.user.js
@@ -20,3 +20,38 @@
  *      - href="mailto:.." for the email and
  *      - href=".." target="_blank" for the website.
  *  *******************************************************/
+
+export class User {
+    constructor(data) {
+        this.id = data.id;
+        this.name = data.name;
+        this.username = data.username;
+        this.email = data.email;
+        this.website = data.website;
+        this.posts = [];
+    }
+
+    addPost(post) {
+        this.posts.push(post);
+    }
+
+    print() {
+        return `
+      <article class="user" data-user-id="${this.id}">
+        <h2>${this.name} (@${this.username})</h2>
+        <p>
+          Email: <a href="mailto:${this.email}">${this.email}</a><br>
+          Website: <a href="https://${this.website}" target="_blank">${this.website}</a>
+        </p>
+
+        <button class="toggle-posts" data-user-id="${this.id}">
+          Posts anzeigen
+        </button>
+
+        <div class="posts hidden" id="posts-${this.id}">
+          ${this.posts.map(post => post.print()).join("")}
+        </div>
+      </article>
+    `;
+    }
+}
